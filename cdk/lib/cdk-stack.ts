e727e97d409b71717d7b25c900c7e10638ded7cc
Mariah Gonzales <mariahsgonzales@gmail.com>
2026-03-13 11:42:41 -0700

    Created DynamoDB table for stock movers

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib/core';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+    // Defining table schema with primary keys: (date, ticker)
+    const stocksTable = new dynamodb.Table(this, 'StocksTable', {
+        partitionKey: {
+            name: 'date',
+            type: dynamodb.AttributeType.STRING,
+        },
+        sortKey: {
+            name: 'ticker',
+            type: dynamodb.AttributeType.STRING,
+        },
+        billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, //charge per request
+        removalPolicy: cdk.RemovalPolicy.DESTROY //deletes table upon "cdk destroy" --> prevents resources from running
+    });
   }
 }
