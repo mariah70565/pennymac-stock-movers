b87a35855979763018bb61b1d3c30aadb4fafef1
Mariah Gonzales <mariahsgonzales@gmail.com>
2026-03-15 16:31:44 -0700

    added aws sdk secrets mocking

--- lambda/fetchHighestStockMover/index.test.ts
+++ lambda/fetchHighestStockMover/index.test.ts
@@ -1,13 +1,30 @@
 import { handler } from "./index";
 import { ScheduledEvent } from 'aws-lambda';
+import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
 
 jest.mock('@massive.com/client-js', () => ({
-  restClient: () => ({
-    getGroupedStocksAggregates: async () => ({
-      results: []
+    restClient: () => ({
+        getGroupedStocksAggregates: async () => ({
+            results: []
+        })
     })
-  })
 }), { virtual: true });
+
+
+// mock AWS SDK Secrets Manager
+jest.mock('@aws-sdk/client-secrets-manager', () => {
+    return {
+        SecretsManagerClient: jest.fn().mockImplementation(() => ({
+            send: jest.fn(async (command: any) => {
+                // return a fake secret
+                if (command instanceof GetSecretValueCommand) {
+                    return { SecretString: 'fake-api-key' };
+                }
+            })
+        })),
+        GetSecretValueCommand: jest.requireActual('@aws-sdk/client-secrets-manager').GetSecretValueCommand
+    };
+});
 
 process.env.STOCKS_TABLE_NAME = "test-table";
 process.env.MASSIVE_API_KEY_SECRET_NAME = "test-secret";
