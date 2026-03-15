9410e9fda92721c01d5d86c4b44300813cde606a
Mariah Gonzales <mariahsgonzales@gmail.com>
2026-03-15 12:35:07 -0700

    Added Secrets Manager Integration for Massive API Key

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -4,6 +4,7 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch'
 import * as events from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import path from 'path';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -12,7 +13,7 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // defining table schema with primary keys: (date, ticker)
+    // +++++++++ defining table schema with primary keys: (date, ticker) +++++++++
     const stocksTable = new dynamodb.Table(this, 'StocksTable', {
         partitionKey: {
             name: 'date',
@@ -26,18 +27,38 @@ export class CdkStack extends cdk.Stack {
         removalPolicy: cdk.RemovalPolicy.DESTROY //deletes table upon "cdk destroy" --> prevents resources from running
     });
 
-    // defining FetchHighestStockMover lambda Function
-    const fetchHighestStockMover = new lambda.Function(this, 'FetchHighestStockMover', {
-        runtime: lambda.Runtime.NODEJS_24_X,
-        handler: 'index.handler',
-        code: lambda.Code.fromAsset(path.join(__dirname, '../../lambda/fetchHighestStockMover')), //where lambda code is located
-        timeout: cdk.Duration.seconds(3) //to signal Cloudwatch alarm
+    // +++++++++ fetching API key for Massive API from GitHub Secrets and storing in Secrets Manager +++++++++
+    // if API key doesn't exist, throw error to prevent stack from deploying without API key secret
+    if (!process.env.MASSIVE_API_KEY) {
+        throw new Error("Failed to create API key secret");
+    }
+
+    const massiveApiKeySecret = new secretsmanager.Secret(this, 'MassiveApiKeySecret', {
+        secretName: 'massive-api-key',
+        secretStringValue: cdk.SecretValue.unsafePlainText(process.env.MASSIVE_API_KEY!)
     });
 
-    // give fetchStockkMovers permission to write to stocksTable
+    // +++++++++ defining FetchHighestStockMover lambda Function +++++++++
+    const fetchHighestStockMover = new lambda.Function(this, 'FetchHighestStockMover', {
+        runtime: lambda.Runtime.NODEJS_24_X,
+        handler: 'fetchHighestStockMover/index.handler', //where lambda handler lives
+        code: lambda.Code.fromAsset(path.join(__dirname, '../../lambda')), //where lambda dependencies are stored
+        timeout: cdk.Duration.seconds(7), //to signal Cloudwatch alarm
+        environment: {
+            STOCKS_TABLE_NAME: stocksTable.tableName, //pass stocks table name to lambda environment variables
+            MASSIVE_API_KEY_SECRET_NAME: massiveApiKeySecret.secretName //pass secret name to lambda environment variables
+        }
+    });
+
+    // ++++++++ granting permissions to FetchHighestStockMover lambda function +++++++++
+    // give fetchStockMovers permission to write to stocksTable
     stocksTable.grantWriteData(fetchHighestStockMover);
 
-    // defining FetchHighestStockMover EventBridge rule to trigger lambda
+    // give fetchStockMovers permission to read API key from Secrets Manager
+    massiveApiKeySecret.grantRead(fetchHighestStockMover);
+    
+    // ++++++++ setting up EventBridge to trigger FetchHighestStockMover lambda every 24 hours +++++++++
+    // defining FetchHighestStockMover EventBridge rule
     const scheduleRule = new events.Rule(this, 'FetchHighestStockMoverSchedule', {
         schedule: events.Schedule.cron({ minute: '0', hour: '20' }) // trigger every 24 hours at 8pm UTC (when stock market closes)
     });
@@ -45,7 +66,7 @@ export class CdkStack extends cdk.Stack {
     // connecting EventBridge rule to FetchHighestStockMover lambda function
     scheduleRule.addTarget(new targets.LambdaFunction(fetchHighestStockMover));
 
-    // setting up Cloudwatch alarm for FetchStockMovers lambda function
+    // ++++++++ setting up Cloudwatch alarm for FetchStockMovers lambda function +++++++++
     if (fetchHighestStockMover.timeout) {
         new cloudwatch.Alarm(this, 'FetchStockMoverAlarm', {
             metric: fetchHighestStockMover.metricDuration().with({ //track the longest Lambda execution time
--- lambda/fetchHighestStockMover/index.ts
+++ lambda/fetchHighestStockMover/index.ts
@@ -1,7 +1,29 @@
 import {Context} from 'aws-lambda'
+import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
+
+const secretsManager = new SecretsManagerClient({});
 
 export const handler = async (event: any, context: Context) => {
     console.log("Fetching highest stock mover...")
+
+    // fetch Massive API key from Secrets Manager
+    try {
+        const command = new GetSecretValueCommand({
+            SecretId: process.env.MASSIVE_API_KEY_SECRET_NAME!
+        });
+
+        const response = await secretsManager.send(command);
+        if (!response.SecretString) {
+            throw new Error("Massive API key value missing")
+        }
+        const massiveApiKey = response.SecretString;
+
+        console.log("API Key retrieved");
+
+    } catch (error) {
+        console.error("Error retrieving API key:", error);
+        throw new Error("Failed to retrieve API key");
+    }
 
     return {
         statusCode: 200,
