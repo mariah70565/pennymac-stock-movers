1b4c894bd507a41feee4401f15f4d6a71fb48139
Mariah Gonzales <mariahsgonzales@gmail.com>
2026-03-17 11:00:34 -0700

    frontend setup with react tsx and chart.js installations

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='flex justify-center text-3xl font-bold'>This is Stock Movers</p>
+    </>
+  )
+}
+
+export default App
