80394ebf9ddec8a0bed66d3ac13db1c7c2999230
Mariah Gonzales <mariahsgonzales@gmail.com>
2026-03-17 19:29:19 -0700

    added additional comments to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,11 +16,11 @@ function App() {
     const [loading, setLoading] = useState(true) //indicated when the app is in the process of fetching data from the API, used to show a loading state to the user
     const [error, setError] = useState<string | null>(null) //stores any error messages that occur during the data fetching process, used to display error information to the user
 
+    // call getMovers API with token (if available) and return 7 movers
     const fetchMovers = async (token?: string | null) => {
         try {
-            setLoading(true)
-            const url = token ? `${API_URL}?nextPageToken=${token}` : API_URL
-            console.log('Fetching movers from URL:', url)
+            setLoading(true) //until movers are fetched
+            const url = token ? `${API_URL}?nextPageToken=${token}` : API_URL //pertains to which group of movers to fetch
             const response = await axios.get<ApiResponse>(url)
             setMovers(response.data.movers)
             setNextPageToken(response.data.nextPageToken || null)
@@ -30,13 +30,14 @@ function App() {
             setError('Failed to fetch movers')
         
         } finally {
-            setLoading(false)
+            setLoading(false) //fetch attempt complete
         }
     }
     useEffect(() => {
         fetchMovers()
     }, [])
 
+    // to grab older movers (next 7 in table)
     const goForward = () => {
         const newPage = currentPage + 1
         setPageHistory([...pageHistory, nextPageToken])
@@ -44,14 +45,16 @@ function App() {
         fetchMovers(nextPageToken)
     }
 
+    // to grab newer movers (back 7 in table)
     const goBack = () => {
         const newPage = currentPage - 1
         setCurrentPage(newPage)
         fetchMovers(pageHistory[newPage])
     }
 
-    const [theme, setTheme] = useState<'light' | 'dark'>('light')
+    const [theme, setTheme] = useState<'light' | 'dark'>('light') //holds theme setting
 
+    // allow users to manually toggle theme
     const toggleTheme = () => {
         const newTheme = theme === 'light' ? 'dark' : 'light'
         setTheme(newTheme)
@@ -59,11 +62,14 @@ function App() {
         localStorage.setItem('theme', newTheme)
     }
 
+    // prevent theme reset on refresh
     useEffect(() => {
-        const savedTheme = localStorage.getItem('theme') as 'light' | 'dark' | null
-        if (savedTheme) {
+        const savedTheme = localStorage.getItem('theme') as 'light' | 'dark' | null //save the theme to retrive on refresh
+        if (savedTheme) { //set theme if any
             setTheme(savedTheme as 'light' | 'dark')
             document.documentElement.classList.toggle('dark', savedTheme === 'dark')
+        
+        // default to user's OS theme settings (dark since light is automatic) if no theme is manually selected
         } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
             setTheme('dark')
             document.documentElement.classList.add('dark')
@@ -72,9 +78,12 @@ function App() {
     
     return (
         <div className='h-full bg-white dark:bg-indigo-950 p-4'>
+            {/* page header */}
             <div className='flex flex-col items-center mb-8 gap-2'>
+                {/* page title */}
                 <h1 className='text-3xl font-bold text-center whitespace-nowrap text-violet-600 dark:text-violet-300'>Stock Movers Dashboard</h1>
-    
+
+                {/* theme toggler */}
                 <button
                     className='flex items-center gap-2 px-4 py-2 text-base rounded-3xl border-4 border-indigo-950 text-violet-300 bg-indigo-950 shadow-md hover:border-violet-400 transition-border duration-150 dark:text-violet-600 dark:bg-white'
                     onClick={toggleTheme}
@@ -84,10 +93,16 @@ function App() {
                 </button>
             </div>
 
+            {/* page content */}
+            {/* getMovers fetch in progress */}
             {loading ? (
                 <p className='flex justify-center text-2xl text-violet-600'>Loading...</p>
+            
+                // error fetching
             ) : error ? (
                 <p className='flex justify-center text-2xl text-red-500'>{error}</p>
+            
+                // normal case
             ) : (
                 <div className='max-w-5xl mx-auto flex flex-col gap-10'>
                     {/* leaderboard of top movers according to current page */}
@@ -96,6 +111,7 @@ function App() {
                     {/* table of winning stocks */}
                     <div className='rounded-lg overflow-hidden border-4 border-violet-600 dark:border-violet-800 shadow-lg hover:shadow-xl shadow-violet-400 dark:shadow-violet-600 transition-shadow duration-300'>
                         <table className='w-full'>
+                            {/* table header */}
                             <thead className=''>
                                 <tr className='bg-violet-600 dark:bg-violet-800 text-white font-bold text-lg'>
                                     <th className='py-2 px-4 text-left'>Stock</th>
@@ -104,6 +120,7 @@ function App() {
                                     <th className='py-2 px-4 text-left'>Close Price</th>
                                 </tr>
                             </thead>
+                            {/* table content */}
                             <tbody className='divide-y divide-gray-200 dark:divide-slate-600'>
                                 {movers.map((mover) => (
                                     <tr
@@ -112,7 +129,7 @@ function App() {
                                     >
                                         <td className='py-2 px-4 font-bold'>{mover.ticker}</td>
                                         <td className='py-2 px-4'>{mover.date}</td>
-                                        <td className={`py-2 px-4 ${mover.percentChange >= 0 ? 'text-green-500 dark:text-green-400' : 'text-red-500'}`}>{mover.percentChange > 0 ? '+' : ''}{mover.percentChange.toFixed(2)}%</td>
+                                        <td className={`py-2 px-4 ${mover.percentChange >= 0 ? 'text-green-500 dark:text-green-400' : 'text-red-500'}`}>{mover.percentChange > 0 ? '+' : ''}{mover.percentChange.toFixed(2)}%</td> {/* green if positive or no change, red if negative change */}
                                         <td className='py-2 px-4'>${mover.closePrice.toFixed(2)}</td>
                                     </tr>
                                 ))}
@@ -122,18 +139,20 @@ function App() {
 
                     {/* table pagination */}
                     <div className='flex items-center justify-center gap-4'>
+                        {/* prev button grabs up to 7 older movers, if any, from table */}
                         <button
                             className='flex items-center gap-2 px-4 py-2 text-base rounded-3xl bg-violet-500 dark:bg-violet-800 hover:bg-violet-800 dark:hover:bg-violet-600 text-white transition-colors duration-150 disabled:opacity-50 disabled:hover:bg-violet-600 dark:disabled:hover:bg-violet-800 disabled:hover:cursor-not-allowed'
                             onClick={goBack}
-                            disabled={currentPage === 0}
+                            disabled={currentPage === 0} //disable going back if on newest 7
                         >
                             <IoArrowBackOutline />
                             Newer
                         </button>
+                        {/* next button grabs up to 7 newer movers, if any, from table */}
                         <button
                             className='flex items-center gap-2 px-4 py-2 text-base rounded-3xl bg-violet-500 dark:bg-violet-800 hover:bg-violet-800 dark:hover:bg-violet-600 text-white transition-colors duration-150 disabled:opacity-50 disabled:hover:bg-violet-600 dark:disabled:hover:bg-violet-800 disabled:hover:cursor-not-allowed'
                             onClick={goForward}
-                            disabled={!nextPageToken}
+                            disabled={!nextPageToken} //disable going next if no next token available (no older movers in stock)
                         >
                             Older
                             <IoArrowForwardOutline />
